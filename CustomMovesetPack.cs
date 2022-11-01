5f803f8f7b45299e58d03d6e8f011b6f0de64615
Eivind <ehaugw@gmail.com>
2022-11-01 15:42:31 +0100

    include maulshove

--- CustomMovesetPack.cs
+++ CustomMovesetPack.cs
@@ -28,7 +28,7 @@
                 new Tuple<int, string[]>(IDs.steelSabreID,      new string[]{IDs.FinesseTag}),
                 new Tuple<int, string[]>(IDs.macheteID,         new string[]{IDs.FinesseTag}),
                 new Tuple<int, string[]>(IDs.goldenMacheteID,   new string[]{IDs.FinesseTag}),
-                new Tuple<int, string[]>(IDs.marbleSwordID,     new string[]{IDs.BastardTag}),
+                new Tuple<int, string[]>(IDs.marbleSwordID,     new string[]{IDs.BastardTag, IDs.MaulShoveTag}),
                 new Tuple<int, string[]>(IDs.marbleAxeID,       new string[]{IDs.BastardTag}),
                 new Tuple<int, string[]>(IDs.oldLegionGladiusID,new string[]{IDs.FinesseTag}),
                 new Tuple<int, string[]>(IDs.deserKhopeshID,    new string[]{IDs.FinesseTag}),
@@ -38,13 +38,13 @@
                 new Tuple<int, string[]>(IDs.brutalClubID,      new string[]{IDs.BastardTag}),
                 new Tuple<int, string[]>(IDs.obsidianSwordID,   new string[]{IDs.BastardTag}),
                 new Tuple<int, string[]>(IDs.brutalAxeID,       new string[]{IDs.BastardTag}),
-                new Tuple<int, string[]>(IDs.prayerClaymoreID,  new string[]{IDs.HolyTag}),
+                new Tuple<int, string[]>(IDs.prayerClaymoreID,  new string[]{IDs.HolyTag, IDs.MaulShoveTag}),
                 new Tuple<int, string[]>(IDs.wolfAxeID,         new string[]{IDs.HolyTag}),
 
                 new Tuple<int, string[]>(IDs.fangSwordID,       new string[]{IDs.FinesseTag}),
 
                 new Tuple<int, string[]>(IDs.palladiumAxeID,  new string[]{IDs.BastardTag, IDs.HolyTag}),
-                new Tuple<int, string[]>(IDs.palladiumClaymoreID,  new string[]{IDs.HolyTag}),
+                new Tuple<int, string[]>(IDs.palladiumClaymoreID,  new string[]{IDs.HolyTag, IDs.MaulShoveTag}),
                 new Tuple<int, string[]>(IDs.palladiumGreataxeID,  new string[]{IDs.HolyTag}),
                 new Tuple<int, string[]>(IDs.palladiumGreathammerID,  new string[]{IDs.HolyTag}),
                 new Tuple<int, string[]>(IDs.palladiumHalberdID,  new string[]{IDs.HolyTag}),
@@ -52,7 +52,8 @@
                 new Tuple<int, string[]>(IDs.palladiumMaceID,  new string[]{IDs.BastardTag, IDs.HolyTag}),
                 new Tuple<int, string[]>(IDs.palladiumShieldID,  new string[]{IDs.HolyTag}),
                 new Tuple<int, string[]>(IDs.palladiumSpearID,  new string[]{IDs.HolyTag}),
-                new Tuple<int, string[]>(IDs.palladiumSwordID,  new string[]{IDs.BastardTag, IDs.HolyTag}),
+                new Tuple<int, string[]>(IDs.palladiumSwordID,  new string[]{IDs.BastardTag, IDs.HolyTag, IDs.MaulShoveTag}),
+                new Tuple<int, string[]>(IDs.oldLanternID,  new string[]{IDs.HandsFreeTag}),
 
         }) if (ResourcesPrefabManager.Instance.GetItemPrefab(tup.Item1) is Item item) CustomItems.SetItemTags(item, tup.Item2, false);
         }
